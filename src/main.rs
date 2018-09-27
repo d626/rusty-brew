ac6743e8888a9018303f1fecd604615b81e4ecca
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2018-09-27 08:44:20 +0200

    Did more work, specs changed, so will start a complete rewrite.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use cortex_m_rt::entry;
 
 extern crate cortex_m;
 
-extern crate nrf52_hal;
+extern crate nrf52832_hal;
 
 fn busy_wait() {
     for _ in 0..100000 {
@@ -18,11 +18,8 @@ fn busy_wait() {
 
 #[entry]
 fn main() -> ! {
-    let p = nrf52_hal::nrf52::Peripherals::take().unwrap();
+    let p = nrf52832_hal::nrf52::Peripherals::take().unwrap();
     let p0 = p.P0;
-    let _y;
-    let x = 42;
-    _y = x;
 
     p0.dirset.modify(|_r, w| w.pin17().set_bit());
     loop {
