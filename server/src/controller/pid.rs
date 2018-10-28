435e7cbbed8b4c1f6a8d01f85d3b772b7bb82b14
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2018-10-28 22:12:57 +0100

    Implement PID

--- server/src/controller/pid.rs
+++ server/src/controller/pid.rs
@@ -1,18 +1,60 @@
+use std::default::Default;
+
 struct Reference(i32);
 
-fn pid(kp: f32, ki: f32, kd: f32) -> f32 {
-    unimplemented!();
+#[derive(Clone)]
+pub struct PidParameters {
+    kp: f32,
+    ki: f32,
+    kd: f32,
 }
 
-#[cfg(test)]
-mod test {
-    use super::mock::*;
+impl PidParameters {
+    pub fn new(kp: f32, ki: f32, kd: f32) -> PidParameters {
+        PidParameters{
+            kp,
+            ki,
+            kd,
+        }
+    }
 
-    fn test() {
-        let mock_sensor = MockTemperatureSensor::new();
-        let mock_output = MockOutput::new();
-
-        mock_sensor.read();
-        mock_output.set();
+    pub fn get_parameters(&self) -> (f32, f32, f32) {
+        (self.kp, self.ki, self.kd)
     }
 }
+
+impl Default for PidParameters {
+    /// kp = 1, ki = 0, kd = 0
+    fn default() -> PidParameters {
+        PidParameters {
+            kp: 1.0,
+            ki: 0.0,
+            kd: 0.0,
+        }
+    }
+}
+
+pub struct Pid {
+    parameters: PidParameters,
+    accumulator: f32,
+    previous_input: f32,
+}
+
+impl Pid {
+    pub fn new(parameters: &PidParameters) -> Pid {
+        Pid {
+            parameters: parameters.clone(),
+            accumulator: 0.0,
+            previous_input: 0.0,
+        }
+    }
+    pub fn pid(&mut self, input: f32, reference: f32) -> f32 {
+        let (kp, ki, kd) = self.parameters.get_parameters();
+        let error = reference - input;
+        self.accumulator += error;
+        let derivative = input - self.previous_input;
+        self.previous_input = input;
+        kp*error + ki*self.accumulator + kd*derivative
+    }
+}
+
